15cf6bafad2830e7cb7bf52b2b620a625f7f518e
Olivier <olivier.winkler@icloud.com>
2023-04-21 18:30:00 +0200

    feat(app): add teaser png for metadata

--- src/pages/api/membercard.ts
+++ src/pages/api/membercard.ts
@@ -8,9 +8,11 @@ export default function handler(
   req: NextApiRequest,
   res: any
 ) {
-  const image = `<svg height="100" width="100" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-  </svg>`
+  const image = `
+    <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+    </svg>
+  `
   var url = "data:image/svg+xml;base64," + Buffer.from(image, "utf-8").toString("base64")
 
   //res.setHeader('Content-Type', 'image/svg+xml');
